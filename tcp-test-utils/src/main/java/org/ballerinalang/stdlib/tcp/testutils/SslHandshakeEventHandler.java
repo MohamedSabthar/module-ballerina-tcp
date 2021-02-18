bb3a53511660285b655b993f55f95931f6fef4c1
MohamedSabthar <sabtharugc0@gmail.com>
2021-02-18 12:00:31 +0530

    add log statements

--- tcp-test-utils/src/main/java/org/ballerinalang/stdlib/tcp/testutils/SslHandshakeEventHandler.java
+++ tcp-test-utils/src/main/java/org/ballerinalang/stdlib/tcp/testutils/SslHandshakeEventHandler.java
@@ -3,8 +3,12 @@ package org.ballerinalang.stdlib.tcp.testutils;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.ssl.SslHandshakeCompletionEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 class SslHandshakeEventHandler extends ChannelInboundHandlerAdapter {
+
+    private static final Logger log = LoggerFactory.getLogger(SslHandshakeEventHandler.class);
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
@@ -16,10 +20,12 @@ class SslHandshakeEventHandler extends ChannelInboundHandlerAdapter {
         if (evt instanceof SslHandshakeCompletionEvent) {
             if (((SslHandshakeCompletionEvent) evt).isSuccess()) {
                 System.out.println("Handshake success" + ctx.channel().remoteAddress());
+                log.info("Handshake success" + ctx.channel().remoteAddress());
                 ctx.pipeline().addLast(new SecureServerHandler());
                 ctx.pipeline().remove(this);
             } else {
                 System.out.println("Handshake failure" + ctx.channel().remoteAddress());
+                log.error("Handshake failure" + ctx.channel().remoteAddress(), ((SslHandshakeCompletionEvent) evt).cause());
             }
         }
     }
@@ -27,6 +33,7 @@ class SslHandshakeEventHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
         System.out.println(cause.getMessage() + ctx.channel().remoteAddress());
+        log.error(cause.getMessage(), cause);
         ctx.close();
     }
 }
